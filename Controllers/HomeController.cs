aaafd4415d10e3e1f1eb9cb7340181f408dfd237
11aaaaqa <11aaaaqa@gmail.com>
2023-04-09 12:02:45 +0400

    MvcSQL
    First ASP.NET Core project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcSQL.Domain;
+using MvcSQL.Models;
+
+namespace MvcSQL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ArticlesRepository articlesRepository;
+
+        public HomeController(ArticlesRepository articlesRepository)
+        {
+            this.articlesRepository = articlesRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = articlesRepository.GetArticles();
+            return View(model);
+        }
+
+        public IActionResult ArticlesEdit(Guid id)
+        {
+            Article model = id == default ? new Article() : articlesRepository.GetArticlesById(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ArticlesEdit(Article model)
+        {
+            if (ModelState.IsValid)
+            {
+                articlesRepository.SaveArticles(model);
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult ArticlesDelete(Guid id)
+        {
+            articlesRepository.DeleteArticles(new Article{Id = id});
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Domain/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace MvcSQL.Domain
+{
+    public class Article
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название статьи")]
+        public string Title { get; set; }
+
+        [Display(Name ="Содержание статьи")]
+        public string Text { get; set; }
+    }
+}
